6cb39cff33c2e713458e87804206cff46e61bcbd
Jim Fairman <fairman.jim@gmail.com>
2018-02-28 08:48:34 -0800

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -11,6 +11,19 @@ sudo pacman -Syu
 #sudo pacman -Sy xf86-input-vmmouse
 #sudo systemctl enable vmtoolsd
 
+# Packages for Laptop Install Power Management
+sudo pacman -S ethtool smartmontools
+sudo pacman -S tlp tlp-rdw
+sudo systemctl enable tlp.service
+sudo systemctl enable tlp-sleep.service
+sudo systemctl enable NetworkManager-dispatcher.service
+sudo systemctl mask systemd-rfkill.service
+sudo systemctl mask systemd-rfkill.socket
+
+# Packages Specific to Macbook Pro Early 2015 Retina
+sudo pacman -S xf86-video-intel mesa-libgl libva-intel-driver libva
+sudo pacman -S xf86-input-synaptics
+
 # Install Programs
 sudo pacman -S chromium firefox vim libreoffice-fresh git gimp krita vlc screen tmux pymol rsync unison openssh
 
@@ -23,13 +36,16 @@ sudo pacman -S r hdf5 hdf5-openmpi gcc-fortran
 # Install UI Elements
 sudo pacman -S arc-kde kvantum-theme-arc
 sudo pacman -S papirus-icon-theme
-pacaur -S ttf-dejavu ttf-google-fonts-typewolf
 
-# Install PacAur & Yaourt
-sudo pacman -S pacaur yaourt
+# Install PacAur
+pacman -S pacaur
+
+# Install Fonts
+sudo pacman -S ttf-dejavu ttf-droid ttf-fira-mono ttf-fira-sans ttf-liberation ttf-linux-libertine-g ttf-tlwg ttf-ubuntu-font-family
+pacaur -S ttf-dejavu ttf-google-fonts-typewolf ttf-noto
 
 # Install Programming IDEs
-pacaur -S visual-studio-code
+pacaur -S visual-studio-code-bin
 pacaur -S rstudio-desktop-bin
 
 # List of KDE Widgets To Get:
