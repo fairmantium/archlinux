127cd7099015a4002019677724c8c3e1d60ad630
Jim Fairman <fairman.jim@gmail.com>
2018-04-29 16:15:46 -0700

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
 # Set Filesystem Size To Be Bigger To Hold Git
-mount -o remount,size=2G /run/archiso/cowspace
-pacman -Sy git
-git clone git://github.com/helmuthdu/aui
+# mount -o remount,size=2G /run/archiso/cowspace
+# pacman -Sy git
+# git clone git://github.com/helmuthdu/aui
 
-cd <dir> && ./fifo
+# ./fifo
+# Reboot!
+# ./lilo
+
+#######################################
+## Once you are in your full install ##
+#######################################
+
+# Update Package Sources
+sudo pacman -Syu --noconfirm
+sudo pacaur -Sy
+
+# GTKMM3 For Copy/Paste Functionality
+sudo pacman -S gtkmm3 --noconfirm
+# Tmux and CRON
+sudo pacman -S tmux cron
+# Stuff For Compiling In R
+sudo pacman -S hdf5 hdf5-openmpi gcc-fortran
+# KDE UI Stuff
+sudo pacman -S arc-kde papirus-icon-theme
+
+# RStudio Install
+pacaur -S rstudio-desktop-bin
